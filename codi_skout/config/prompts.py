063325c719f232925d01697671d67258d10feddc
Sam_dev <102005672+Samilincoln@users.noreply.github.com>
2026-07-15 01:15:07 +0100

    fix: PromptTemplate fix

--- codi_skout/config/prompts.py
+++ codi_skout/config/prompts.py
@@ -1,4 +1,4 @@
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 
 CODE_REVIEW_PROMPT = PromptTemplate(
     input_variables=["languages", "file_structure", "code_samples", "endpoints"],
